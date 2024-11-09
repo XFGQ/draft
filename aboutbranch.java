82a6d9ea9494811f028942a855529b31e6716100
XFGQ <furkanduman1925@gmail.com>
2024-11-09 22:22:08 +0300

    Burada vscode'ta değişiklik yaptıktan sonra terminal ile nasıl oluşturudğum branch'i githuba yolladığımızı yazıyorum

---
+++ aboutbranch.java
@@ -0,0 +1,13 @@
+-----BRANCH OLUŞTURMA
+git checkout -b branchname
+
+git branch
+//yazdıktan sonra ekranda aşağıdaki gibi branchlar listenecektir
+
+----PUSHLAMA
+
+git add.
+
+git commit -m "yorumun "
+
+git push origin branchname
